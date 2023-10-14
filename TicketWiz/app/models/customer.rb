d397c6b545f986561653c49f858fa74c2f2afd2b
Alex <alexkimanthi92@gmail.com>
2023-10-14 12:15:38 +0300

    add validations to customer model

--- TicketWiz/app/models/customer.rb
+++ TicketWiz/app/models/customer.rb
@@ -3,4 +3,18 @@ class Customer < ApplicationRecord
     has_many :events  
     has_many :calendar_events 
     has_many :tickets   
+
+    validate :password_complexity
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+    validates :age, presence: true, numericality: { greater_than_or_equal_to: 18 }
+    validates :first_name, presence: true
+    validates :last_name, presence: true 
+
+    private
+
+    def password_complexity
+        unless password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\.).{6,}$/)
+          errors.add(:password, "must include at least one lowercase letter, one uppercase letter, one digit, one period, and be at least 6 characters long.")
+        end
+    end
 end
