5e5e3a4d20dc927dab733da4ff2826d039f4c634
Ken <kabutukenneth3@gmail.com>
2023-10-14 18:00:08 +0300

    add create action in calendar_events controller

--- TicketWiz/app/controllers/calendar_events_controller.rb
+++ TicketWiz/app/controllers/calendar_events_controller.rb
@@ -2,8 +2,20 @@ class CalendarEventsController < ApplicationController
 
     before_action :authenticate_user
 
+    # Display a list of calendar events for the current user
     def index
         @calendar_events = current_user.calendar_events
+    end
+
+    # Create a calendar event for the current user
+    def create
+        @calendar_event = current_user.calendar_events.build(calendar_event_params)
+
+        if @calendar_event.save
+            render json: @calendar_event, status: :created
+        else 
+            render json: @calendar_event.errors, status: :unprocessable_entity
+        end
     end
 
 end
