61f36cedb81a14de3af8619f8ff83ea3f404aedb
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-10-14 18:09:32 +0300

    CRUD operations to payments_controller.rb

--- TicketWiz/app/controllers/payments_controller.rb
+++ TicketWiz/app/controllers/payments_controller.rb
@@ -4,9 +4,10 @@ class PaymentsController < ApplicationController
     def index
       @payments = Payment.all
     end
+    
   
     def show
-        
+
         @payments = Payment.find(params[:id])
     end
   
