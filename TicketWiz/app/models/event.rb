1a16c49d7f6a55ff1b6f6522eec64a92af04e5a9
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-10-14 11:55:03 +0300

    Created event model Added the migrations Created payment model added the migrations Created events_controller.rb Created payments_controller.rb added validations to event model added validations to payment model added CRUD operations to events_controller.rb added CRUD operations to payments_controller.rb modified the routes

--- TicketWiz/app/models/event.rb
+++ TicketWiz/app/models/event.rb
@@ -1,3 +1,11 @@
 class Event < ApplicationRecord
   belongs_to :organiser
+  has_many :tickets
+
+   # Validations
+   validates :title, presence: true
+   validates :date, presence: true
+   validates :start_time, presence: true
+   validates :end_time, presence: true
+   validates :date, date: { after_or_equal_to: -> { Date.current }, message: 'must be in the future' }
 end
