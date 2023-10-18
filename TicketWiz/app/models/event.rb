8653e1f6202be4775fd89288b41a4f04e1e2547f
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-10-18 20:57:18 +0300

    modified event.rb

--- TicketWiz/app/models/event.rb
+++ TicketWiz/app/models/event.rb
@@ -9,4 +9,28 @@ class Event < ApplicationRecord
    validates :start_time, presence: true
    validates :end_time, presence: true
    validates :date, date: { after_or_equal_to: -> { Date.current }, message: 'must be in the future' }
-end
+
+   CATEGORIES = [
+    "Conferences",
+    "Workshops and Seminars",
+    "Networking Events",
+    "Trade Shows",
+    "Concerts and Music Festivals",
+    "Sporting Events",
+    "Art Exhibitions and Galleries",
+    "Food and Beverage Events",
+    "Theater and Performing Arts",
+    "Community and Charity Events",
+    "Parties",
+    "Educational Events",
+    "Tech and Hackathons",
+    "Adventure Activities",
+    "Cultural",
+    "Virtual",
+    "Cosplay",
+    "Gaming"
+  ]
+
+  validates :category, inclusion: { in: CATEGORIES }
+
+  end
