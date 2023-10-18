99e93e099599e074c21bd1b748ea2054e3fe98bf
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-10-18 18:42:01 +0300

    Merge changes from main into development

--- TicketWiz/db/seeds.rb
+++ TicketWiz/db/seeds.rb
@@ -1,7 +1,78 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+{
+  "organisers": [
+    {
+      "first_name": "John",
+      "last_name": "Doe",
+      "email": "john@example.com",
+      "password": "Password1.",
+      "phone_number": "1234567890",
+      "age": 30
+    },
+    {
+      "first_name": "Jane",
+      "last_name": "Smith",
+      "email": "jane@example.com",
+      "password": "Password2.",
+      "phone_number": "9876543210",
+      "age": 25
+    }
+  ],
+  "customers": [
+    {
+      "first_name": "Alice",
+      "last_name": "Johnson",
+      "email": "alice@example.com",
+      "password": "Password3.",
+      "phone_number": "5555555555",
+      "age": 28
+    },
+    {
+      "first_name": "Bob",
+      "last_name": "Wilson",
+      "email": "bob@example.com",
+      "password": "Password4.",
+      "phone_number": "6666666666",
+      "age": 22
+    }
+  ],
+  "events": [
+    {
+      "title": "Music Concert",
+      "date": "2023-11-15",
+      "start_time": "18:00:00",
+      "end_time": "22:00:00",
+      "organiser_id": 1
+    },
+    {
+      "title": "Art Exhibition",
+      "date": "2023-12-10",
+      "start_time": "10:00:00",
+      "end_time": "16:00:00",
+      "organiser_id": 2
+    }
+  ],
+  "tickets": [
+    {
+      "event_id": 1,
+      "customer_id": 1
+    },
+    {
+      "event_id": 2,
+      "customer_id": 2
+    }
+  ],
+  "payments": [
+    {
+      "amount": 50.00,
+      "transaction_id": "ABC123",
+      "status": "completed",
+      "ticket_id": 1
+    },
+    {
+      "amount": 30.00,
+      "transaction_id": "XYZ789",
+      "status": "completed",
+      "ticket_id": 2
+    }
+  ]
+}
