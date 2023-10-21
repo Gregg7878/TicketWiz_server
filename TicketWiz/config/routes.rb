8f6be3e1dcc3e461f7ee6e4d9601821b40d95aa1
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-21 19:13:40 +0300

    Updated routes.rb

--- TicketWiz/config/routes.rb
+++ TicketWiz/config/routes.rb
@@ -15,11 +15,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :events
   resources :payments
+
+  get '/featured_events', to: 'events#featured'
   
   # Defines the root path route ("/")
   # root "articles#index"
 
-  # resources :calendar_events, only: [:index, :create, :destroy]
-  # resources :tickets, only: [:index, :create, :show, :destroy]
+  resources :calendar_events, only: [:index, :create, :destroy]
+  resources :tickets, only: [:index, :create, :show, :destroy]
 
 end
