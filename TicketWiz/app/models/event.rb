bf7ae9fed307f7919611d4d6a4dbfc066cb6e0c6
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-10-21 22:01:06 +0300

    added validation

--- TicketWiz/app/models/event.rb
+++ TicketWiz/app/models/event.rb
@@ -6,7 +6,7 @@ class Event < ApplicationRecord
    # Validations
    #validates :title, presence: true
    #validates :date, presence: true
-   validates :start_time, presence: true
+   #validates :start_time, presence: true
    validates :end_time, presence: true
    validates :date, date: { after_or_equal_to: -> { Date.current }, message: 'must be in the future' }
 
