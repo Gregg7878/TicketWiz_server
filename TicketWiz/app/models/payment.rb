d46e9a4ba3c42624564c3c42df62bc7eab1b65a7
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-21 17:39:55 +0300

    Defined calculate amount and status methods

--- TicketWiz/app/models/payment.rb
+++ TicketWiz/app/models/payment.rb
@@ -7,4 +7,19 @@ class Payment < ApplicationRecord
   validates :status, presence: true, inclusion: { in: %w(pending completed failed) }
   validates :transaction_id, presence: true, uniqueness: true, length: { is: 36 }
   
+  def calculate_amount
+    ticket = self.ticket
+    self.amount = ticket.calculate_price * ticket.quantity
+  end
+
+  def set_as_completed
+    self.status = 'completed'
+    save
+  end
+
+  def set_as_failed
+    self.status = 'failed'
+    save
+  end
+  
 end
