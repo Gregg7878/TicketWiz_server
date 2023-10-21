e84523887149b79a362a62f6a71932b81f82d524
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-21 17:34:12 +0300

    Added method for date to be in future

--- TicketWiz/app/models/event.rb
+++ TicketWiz/app/models/event.rb
@@ -12,5 +12,13 @@ class Event < ApplicationRecord
    validates :date, presence: true
    validates :start_time, presence: true
    validates :end_time, presence: true
-   validates :date, date: { after_or_equal_to: -> { Date.current }, message: 'must be in the future' }
+   validate :date_must_be_in_future
+   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+ 
+   def date_must_be_in_future
+    if date.present? && date <= Date.current
+      errors.add(:date, 'must be in the future and not today')
+    end
+  end
+
 end
