6095ed484bee70c96de58214d9e124c49c2d92f1
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-21 17:32:33 +0300

    Added categories

--- TicketWiz/app/models/event.rb
+++ TicketWiz/app/models/event.rb
@@ -2,6 +2,10 @@ class Event < ApplicationRecord
   belongs_to :organiser
   has_many :tickets, dependent: :destroy
 
+  CATEGORIES = ['Conferences', 'Workshops and Seminars', 'Networking Events', 'Trade Shows', 'Concerts', 'Sporting Events', 'Art Shows', 'Feast Event', 'Performing Arts', 'Charity Events', 'Parties', 'Educational Events', 'Tech and Hackathons', 'Adventure', 'Cultural', 'Virtual', 'Cosplay', 'Gaming']
+
+  validates :category, inclusion: { in: CATEGORIES, message: "is not a valid category" }
+
   
    # Validations
    validates :title, presence: true
