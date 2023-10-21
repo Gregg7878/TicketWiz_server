6fa4225ff9665721ef0728448aadd2246ab70c98
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-21 18:56:39 +0300

    Updated tickets controller

--- TicketWiz/app/controllers/tickets_controller.rb
+++ TicketWiz/app/controllers/tickets_controller.rb
@@ -1,48 +1,54 @@
 class TicketsController < ApplicationController
-
     before_action :authenticate_user
     before_action :set_event, only: [:create]
     before_action :set_ticket, only: [:show, :destroy]
-
-    # display list of tickets for current user
+  
     def index
-        @tickets = current_user.tickets
+      @tickets = current_user.tickets
     end
-
-    # create a new ticket
+  
     def create
+      if @event.available_tickets_count >= params[:ticket][:quantity].to_i
         @ticket = Ticket.new(ticket_params)
+        @ticket.event = @event
+  
         if @ticket.save
+          @event.available_tickets_count -= @ticket.quantity
+          @event.save
+  
           render json: @ticket, status: :created
         else
+          puts @ticket.errors.full_messages
           render json: @ticket.errors, status: :unprocessable_entity
         end
+      else
+        render json: { error: "Not enough available tickets for this quantity" }, status: :unprocessable_entity
+      end
     end
-
-    # display details of a specific ticket by its ID
+  
     def show
-        @ticket = Ticket.find(params[:id])
+      @event_date = @ticket.event.date
+      @ticket_price = @ticket.calculate_price
+      render json: @ticket_price
     end
-
-
-    # delete a ticket by its ID
+  
     def destroy
-        @ticket = Ticket.find(params[:id])
-        @ticket.destroy
-        head :no_content
+      @ticket.destroy
+      head :no_content
     end
-
+  
     private
-
+  
     def ticket_params
-        params.require(:ticket).permit(:event_id, :customer_id, :ticket_type, :quantity)
-      end
-    
-      def set_event
-        @event = Event.find(params[:event_id])
-      end
-    
-      def set_ticket
-        @ticket = Ticket.find(params[:id])
-      end
-end
+      params.require(:ticket).permit(:event_id, :customer_id, :ticket_type, :quantity)
+    end
+  
+    def set_event
+      @event = Event.find(params[:event_id])
+    end
+  
+    def set_ticket
+      @ticket = Ticket.find(params[:id])
+    end
+  end
+  
