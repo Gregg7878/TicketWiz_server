480381c1909d5e5e4e7a0707e1cf246dbb1455a3
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-21 17:41:12 +0300

    Defined method to geneate transaction id

--- TicketWiz/app/models/payment.rb
+++ TicketWiz/app/models/payment.rb
@@ -6,7 +6,10 @@ class Payment < ApplicationRecord
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :status, presence: true, inclusion: { in: %w(pending completed failed) }
   validates :transaction_id, presence: true, uniqueness: true, length: { is: 36 }
-  
+
+  before_create :generate_transaction_id
+  before_save :calculate_amount
+
   def calculate_amount
     ticket = self.ticket
     self.amount = ticket.calculate_price * ticket.quantity
@@ -21,5 +24,11 @@ class Payment < ApplicationRecord
     self.status = 'failed'
     save
   end
-  
+
+  private
+
+  def generate_transaction_id
+    self.transaction_id = SecureRandom.uuid
+  end
+
 end
