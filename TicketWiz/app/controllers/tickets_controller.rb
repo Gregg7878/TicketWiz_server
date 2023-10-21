3f5eb5b1d0eef4d431bb5247acde7a2921b63fb2
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-21 17:58:02 +0300

    Added set ticket and set event methods

--- TicketWiz/app/controllers/tickets_controller.rb
+++ TicketWiz/app/controllers/tickets_controller.rb
@@ -1,6 +1,8 @@
 class TicketsController < ApplicationController
 
     before_action :authenticate_user
+    before_action :set_event, only: [:create]
+    before_action :set_ticket, only: [:show, :destroy]
 
     # display list of tickets for current user
     def index
@@ -33,6 +35,14 @@ class TicketsController < ApplicationController
     private
 
     def ticket_params
-        params.require(:ticket).permit(:event_id, :customer_id, :ticket_type, :price, :purchase_date)
-    end
+        params.require(:ticket).permit(:event_id, :customer_id, :ticket_type, :quantity)
+      end
+    
+      def set_event
+        @event = Event.find(params[:event_id])
+      end
+    
+      def set_ticket
+        @ticket = Ticket.find(params[:id])
+      end
 end
