a849a7a5a72a2251e3776a6d964f0651ed951524
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-10-23 12:31:42 +0300

    modified events_controller.rb

--- TicketWiz/app/controllers/payments_controller.rb
+++ TicketWiz/app/controllers/payments_controller.rb
@@ -1,6 +1,6 @@
 class PaymentsController < ApplicationController
     before_action :set_payment, only: [:show, :edit, :update, :destroy]
-    
+  
     def index
       @payments = Payment.all
     end
