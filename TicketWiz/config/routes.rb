6fb8e8e590c8ef0a3e6da75b0562158ba92215a1
Alex <alexkimanthi92@gmail.com>
2023-10-24 21:48:03 +0300

    worked on routes

--- TicketWiz/config/routes.rb
+++ TicketWiz/config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
+  
+  # resources :organisers, only: [:create, :show]
+  # resources :customers, only: [:create, :show]
 
-  # resources :organisers
-  # resources :customers
-
-  # post "/signupOrg", to: "organisers#create" 
-  # post "/signup", to: "customers#create" 
-  # get "/meOrg", to: "organisers#show"
-  # get "/me", to: "customers#show"
-  # post "login", to:"customer_sessions#create" 
-  # delete "/logout", to: "customer_sessions#destroy" 
-  # post "loginOrg", to:"organiser_sessions#create"
-  # delete "/logoutOrg", to: "organiser_sessions#destroy"
+  post "/signupOrg", to: "organisers#create" 
+  post "/signup", to: "customers#create" 
+  get "/meOrg", to: "organisers#show"    
+  get "/me", to: "customers#show"
+  post "login", to:"customer_sessions#create"   
+  delete "/logout", to: "customer_sessions#destroy" 
+  post "loginOrg", to:"organiser_sessions#create"
+  delete "/logoutOrg", to: "organiser_sessions#destroy"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :events
@@ -23,5 +23,7 @@ Rails.application.routes.draw do
 
   resources :calendar_events, only: [:index, :create, :destroy]
   resources :tickets, only: [:index, :create, :show, :destroy]
+
+ 
 
 end
