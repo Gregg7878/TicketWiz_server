93df870067335a5ca7ff9a68b919ba54850266cb
Alex <alexkimanthi92@gmail.com>
2023-10-24 22:25:00 +0300

    made correction on  show  method for bookings

--- TicketWiz/app/controllers/bookings_controller.rb
+++ TicketWiz/app/controllers/bookings_controller.rb
@@ -3,7 +3,7 @@ class BookingsController < ApplicationController
     def index
         bookings = Booking.all 
         render json: bookings
-    end
+    end 
     
     def show
         booking = Booking.find(params[:id])
