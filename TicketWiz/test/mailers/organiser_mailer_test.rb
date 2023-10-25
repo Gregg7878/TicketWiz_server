3653215738990d674bea853d50285763dad66633
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-10-25 10:17:52 +0300

    incorporation of Action mailer

---
+++ TicketWiz/test/mailers/organiser_mailer_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class OrganiserMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ TicketWiz/test/mailers/previews/organiser_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/organiser_mailer
+class OrganiserMailerPreview < ActionMailer::Preview
+
+end
