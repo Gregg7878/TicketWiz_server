c2fc1d655f741e7dc843a2ba034b5794bd179c3d
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-10-26 13:18:16 +0300

    modified event.rb

--- TicketWiz/app/models/event.rb
+++ TicketWiz/app/models/event.rb
@@ -31,7 +31,7 @@ class Event < ApplicationRecord
    #validates :end_time, presence: true
    #validate :date_must_be_in_future
    #validates :date, date: { after_or_equal_to: -> { Date.current }, message: 'must be in the future' }
-   #validates :category, inclusion: { in: CATEGORIES }
+   validates :category, inclusion: { in: CATEGORIES }
 
   end
 
