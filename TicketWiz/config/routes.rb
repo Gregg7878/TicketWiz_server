8ad4aa97dd4cbb4d5469e8d019acd52b023c02cd
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-30 13:21:00 +0300

    Routes update

--- TicketWiz/config/routes.rb
+++ TicketWiz/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  
   resources :mpesas
 
   # resources :organisers
   # resources :customers
-
+  
   post "/signup", to: "customers#create" 
   get "/me", to: "customers#show"
   post "login", to:"customer_sessions#create" 
@@ -12,24 +13,30 @@ Rails.application.routes.draw do
   post "/signupOrg", to: "organisers#create" 
   post "loginOrg", to:"organiser_sessions#create"
   delete "/logoutOrg", to: "organiser_sessions#destroy"
-
-
+  
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :events
   resources :payments
   resources :bookings
-
+  
   get '/featured_events', to: 'events#featured'
-
+  
   post "stkpush", to: "mpesas#stkpush"
-
+  
   get '/organiser_dashboard', to: 'organisers#dashboard'
-
+  
   get '/event_history', to: 'customers#event_history'
-
+  
   get '/ticket_wallet', to: 'customers#ticket_wallet'
-
-
+  
+  
+  
+  # Defines the root path route ("/")
+  # root "articles#index"
+  
+  resources :calendar_events, only: [:index, :create, :destroy]
+  resources :tickets, only: [:index, :create, :show, :destroy]
   
   # Defines the root path route ("/")
   # root "articles#index"
