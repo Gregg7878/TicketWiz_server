4f5428ee07ebde5ae58e5f7d472413e0a63fffe0
Sylvia Mumbi <mumbi099@gmail.com>
2023-10-30 13:49:54 +0300

    Added mumbi branch backup

--- TicketWiz/app/controllers/application_controller.rb
+++ TicketWiz/app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::API
 
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
     # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    # mumbi-backup branch
   
     def render_unprocessable_entity_response(invalid)
       render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
